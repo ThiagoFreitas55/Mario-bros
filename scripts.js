a5848adc2172eb620553656124ce5119192b3088
ThiagoFreitas55 <thiagolps55@gmail.com>
2024-12-23 06:08:17 -0300

    alteração

--- scripts.js
+++ scripts.js
@@ -1,11 +1,13 @@
 const form = document.querySelector(".formulario")
 const masc = document.querySelector(".mascara-formulario")
 const fale = document.querySelector("faleconosco")
+const button = document.querySelector("button")
 
 function cliqueNoBotao(){
     form.style.left = "40%"
     form.style.transform = "translateX (-50%)"
     masc.style.visibility = "visible"
+    button.style.display = "none"
 }
 
 masc.addEventListener("click",enconder)
@@ -13,6 +15,7 @@ masc.addEventListener("click",enconder)
 function enconder(){
     form.style.left = "-320px"
     masc.style.visibility = "hidden"
+    button.style.display = "block"
     
 
 }
@@ -21,6 +24,7 @@ fale.addEventListener("click", abreform)
 
 function  abreform(){
     fale.mostraformulario
+    
 }
 
 
